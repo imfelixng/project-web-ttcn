bafdb8577f335d6fdf689994f68bdb901afaff62
Nguyen Quang An <ngquangan@gmail.com>
2018-11-24 08:35:44 +0700

    show images in question

--- client/src/screens/Newfeed/components/Question.js
+++ client/src/screens/Newfeed/components/Question.js
@@ -1,6 +1,5 @@
 import React, { Component } from 'react'
 import {NavLink} from 'react-router-dom';
-import { EditorState, convertToRaw, ContentState } from 'draft-js';
 import draftToHtml from 'draftjs-to-html';
 export default class Question extends Component {
 
@@ -8,7 +7,8 @@ export default class Question extends Component {
         isOpenFunctional: false,
         currentUserID: '',
         userOther: [],
-        categoryQuestion: []
+        categoryQuestion: [],
+        isShowEdit: false
     }
 
     onOpenFunctional = () => {
@@ -49,43 +49,92 @@ export default class Question extends Component {
         this.props.deleteQuestion(questionID);
     }
 
+    onOpenEdit = () => {
+        this.setState({
+            isShowEdit: !this.state.isShowEdit
+        });
+    }
+
+    showImages = (images) => {
+        
+        if(images.length === 1) {
+            return images.map((image, index) => {
+                return <img 
+                    src = {image.dataURL}
+                    key = {index}
+                    className = "question_image--100"
+                />
+            });
+        }
+
+        if(images.length === 2) {
+            return images.map((image, index) => {
+                return <img 
+                    src = {image.dataURL}
+                    key = {index}
+                    className = "question_image--50"
+                />
+            });
+        }
+
+        if(images.length === 3) {
+            return images.map((image, index) => {
+                return <img 
+                    src = {image.dataURL}
+                    key = {index}
+                    className = "question_image--30"
+                />
+            });
+        }
+
+        if(images.length > 3) {
+            return images.map((image, index) => {
+                return <img 
+                    src = {image.dataURL}
+                    key = {index}
+                    className = "question_image--30"
+                />
+            });
+        }
+    }
+
   render() {
     let {question} = this.props;
     let userInfo = this.state.userOther[question.userID];
     let categoryInfo = this.state.categoryQuestion[question.categoryID];
+    console.log(question);
     return (
       <React.Fragment>
         <div className="post-bar">
             <div className="post_topbar">
-                                        <div className="usy-dt">
-                                        <img className = "user-picy" src= {userInfo ? userInfo.avatar : "/images/users/img_avatar_default.png"}  />
-                                        <div className="usy-name">
-                                            <h3>{userInfo ? userInfo.fullname : "yourname"}</h3>
-                                            <span><img src="images/clock.png"  />3 min ago</span>
-                                        </div>
-                                        </div>
-                                        <div className="ed-opts">
-                                            {
-                                                this.state.currentUserID &&
-                                                <a  className="ed-opts-open" onClick = {this.onOpenFunctional}><i className="la la-ellipsis-v" /></a>
-                                            }
-                                            {
-                                                this.state.isOpenFunctional &&
-                                                <ul className="ed-options active">
-                                                    {
-                                                        this.state.currentUserID === question.userID &&
-                                                        <React.Fragment>
-                                                            <li><a href="#" >Edit Post</a></li>
-                                                            <li><a onClick = {() => this.onDeleteQuestion(question.questionID)} >Delete</a></li>
-                                                        </React.Fragment>
-
-                                                    }
-                                                    <li><a href="#" >UnMarked</a></li>
-                                                    <li><a href="#" >Report</a></li>
-                                                </ul>
-                                            }
-                                        </div>
-                                    </div>
+                <div className="usy-dt">
+                    <img className = "user-picy" src= {userInfo ? userInfo.avatar : "/images/users/img_avatar_default.png"}  />
+                    <div className="usy-name">
+                        <h3>{userInfo ? userInfo.fullname : "yourname"}</h3>
+                        <span><img src="images/clock.png"  />3 min ago</span>
+                     </div>
+                </div>
+                <div className="ed-opts">
+                {
+                    this.state.currentUserID &&
+                    <a  className="ed-opts-open" onClick = {this.onOpenFunctional} ><i className="la la-ellipsis-v" /></a>
+                }
+                {
+                    this.state.isOpenFunctional &&
+                    <ul className="ed-options active">
+                    {
+                        this.state.currentUserID === question.userID &&
+                        <React.Fragment>
+                            <li><a data-toggle="modal" data-target="#exampleModal" onClick = {this.onOpenEdit}>Edit Post</a></li>
+                            <li><a onClick = {() => this.onDeleteQuestion(question.questionID)} >Delete</a></li>
+                        </React.Fragment>
+                    }
+                        <li><a href="#" >UnMarked</a></li>
+                        <li><a href="#" >Report</a></li>
+                    </ul>
+                }
+                </div>
+            </div>
             <div className="epi-sec">
                                         <ul className="descp">
                                         <li>
@@ -110,6 +159,14 @@ export default class Question extends Component {
                                         <div 
                                             className = "question__content" 
                                             dangerouslySetInnerHTML = {this.showContent(question.content)}>
+                                        </div>
+                                        <div
+                                            className = "question_images"
+                                        >
+                                            {
+                                                this.showImages(question.images)
+                                            }
+                                            
                                         </div>
                                         <ul className="skill-tags">
                                             {this.showTags(question.tags)}
@@ -140,6 +197,29 @@ export default class Question extends Component {
                 </div>
             </div>
         </div>{/*post-bar end*/}
+        {
+            this.state.isShowEdit &&
+            <div className="modal fade" id="exampleModal" tabIndex={-1} role="dialog" aria-labelledby="exampleModalLabel" aria-hidden="true">
+                <div className="modal-dialog" role="document">
+                    <div className="modal-content">
+                    <div className="modal-header">
+                        <h5 className="modal-title" id="exampleModalLabel">Modal title</h5>
+                        <button type="button" className="close" data-dismiss="modal" aria-label="Close">
+                        <span aria-hidden="true">×</span>
+                        </button>
+                    </div>
+                    <div className="modal-body">
+                        ...
+                    </div>
+                    <div className="modal-footer">
+                        <button type="button" className="btn btn-secondary" data-dismiss="modal">Cancel</button>
+                        <button type="button" className="btn btn-primary">Save changes</button>
+                    </div>
+                    </div>
+                </div>
+            </div>
+
+        }
       </React.Fragment>
     )
   }
