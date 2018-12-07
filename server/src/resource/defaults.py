075b8fa2deb3bd9528673d9cd93d506320c22567
Nguyen Phu <nvphu1306@gmail.com>
2018-12-07 12:28:36 +0700

    chore: minor update

--- server/src/resource/defaults.py
+++ server/src/resource/defaults.py
@@ -5,4 +5,5 @@ SECRET_KEY = "KERaTjIjgZ5ZFHrRc9r1iFpStKU"
 BASE_PATH = os.path.dirname(os.path.dirname(os.path.dirname(
     os.path.dirname(os.path.abspath(__file__)))))
 
-PUBLIC_PATH = os.path.join(BASE_PATH, 'public')
+# PUBLIC_PATH = os.path.join(BASE_PATH, 'public')
+PUBLIC_PATH = '/app/public'
