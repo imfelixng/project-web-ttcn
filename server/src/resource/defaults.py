c1383f55419a9ee4db2cee12dc702b12f21621eb
Nguyen Phu <nvphu1306@gmail.com>
2018-12-07 14:22:31 +0700

    chore: mirror update

--- server/src/resource/defaults.py
+++ server/src/resource/defaults.py
@@ -6,4 +6,4 @@ BASE_PATH = os.path.dirname(os.path.dirname(os.path.dirname(
     os.path.dirname(os.path.abspath(__file__)))))
 
 # PUBLIC_PATH = os.path.join(BASE_PATH, 'public')
-PUBLIC_PATH = '/app/public'
+PUBLIC_PATH = '/app/project-web-ttcn/public'
