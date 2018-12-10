9d77fcfa1885937bac567362198d160e84f0b989
Nguyen Phu <nvphu1306@gmail.com>
2018-12-10 21:08:36 +0700

    chore: mirror update

--- server/src/resource/apps/question/__init__.py
+++ server/src/resource/apps/question/__init__.py
@@ -100,5 +100,5 @@ def __setup__(module):
 
     @module.endpoint("/delete/<questionID>", methods=["DELETE"])
     def delete_question(questionID):
-        module.data.delete_one("question", questionID)
+        module.data.db.question.delete_one({"questionID": questionID})
         return "ok"
