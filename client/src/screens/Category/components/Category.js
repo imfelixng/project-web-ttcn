779a511a0bdbfe1fc84b44b2035908c71c3ae1ab
Nguyen Quang An <ngquangan@gmail.com>
2018-12-11 16:31:26 +0700

    add route category item

--- client/src/screens/Category/components/Category.js
+++ client/src/screens/Category/components/Category.js
@@ -1,14 +1,16 @@
 import React, { Component } from 'react'
+import {NavLink} from 'react-router-dom';
+
 
 export default class Category extends Component {
   render() {
     return (
       <React.Fragment>
         <div className="post-bar flex">
-        <a className="name-list">{this.props.category.name}</a>
+        <NavLink to = {"/categories/" + this.props.category.categoryID} className="name-list">{this.props.category.name}</NavLink>
             <ul className="bk-links mr-2 my-3">
-              <li className="question-amount"><a href="#"><i class="fa fa-question-circle icon-ques"></i><span className="amount">19</span></a></li>
-              <li><a href="#"><i class="fa fa-plus-square"></i></a></li>
+              <li className="question-amount"><a href="#"><i className="fa fa-question-circle icon-ques"></i><span className="amount">19</span></a></li>
+              <li><a href="#"><i className="fa fa-plus-square"></i></a></li>
             </ul>
         </div>{/*post-bar end*/}
       </React.Fragment>
