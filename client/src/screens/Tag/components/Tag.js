c42a32b66e3565ce1fdbc52a5066df76f0ff87a8
Nguyen Quang An <ngquangan@gmail.com>
2018-12-12 23:19:16 +0700

    fix update question when idTag in url changed

--- client/src/screens/Tag/components/Tag.js
+++ client/src/screens/Tag/components/Tag.js
@@ -8,7 +8,7 @@ export default class Tag extends Component {
         <div className="post-bar flex">
             <NavLink className="name-list" to = {"/tags/" + this.props.tag.id}>{this.props.tag.text}</NavLink>
             <ul className="bk-links mr-2 my-3">
-              <li className="question-amount"><a href="#"><i class="fa fa-question-circle icon-ques"></i><span className="amount">19</span></a></li>
+              <li className="question-amount"><a href="#"><i className="fa fa-question-circle icon-ques"></i><span className="amount">19</span></a></li>
               <li><a href="#"><i className="fa fa-plus-square"></i></a></li>
             </ul>
         </div>{/*post-bar end*/}
