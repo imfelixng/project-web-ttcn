18a4ea49568b0ec09f4938f7f88f7b8d1c75c49a
maithao2206 <ptmt2206@gmail.com>
2018-12-13 11:52:10 +0700

    notification

--- client/src/screens/Notification/components/NotificationScreen.js
+++ client/src/screens/Notification/components/NotificationScreen.js
@@ -1,10 +1,70 @@
-import React, { Component } from 'react'
+import React, { Component } from 'react';
 
 export default class NotificationScreen extends Component {
   render() {
     return (
       <React.Fragment>
-    
+        <div className = "wrapper">
+        <div className="container">
+          <div className="header-notify">
+            <div className="notify-title"><p >Your Notification</p></div>
+            <span className="notify-setting"><a>Notification Settings</a></span>
+          </div>
+          <hr className="under-line"></hr>
+          <div>
+            <ul className="menu-list">
+             <a className="list-item">
+              <li className="list-notify">
+                <img src="/images/resources/s1.png" alt = "logo" />
+                <div className="sgt-text">
+                <div className="detail-notify">
+                <h4>Jessica William</h4>
+                <p className="comment-notify"> đã thích bài viết của bạn</p>
+                </div>
+                <span><i className="fa fa-heart"></i></span>
+                <span className="time-notify">1h</span>
+                </div>
+              </li>
+              <li className="list-notify">
+                <img src="/images/resources/s1.png" alt = "logo" />
+                <div className="sgt-text">
+                <div className="detail-notify">
+                <h4>Jessica William</h4>
+                <p className="comment-notify"> đã thích bài viết của bạn</p>
+                </div>
+                <span><i className="fa fa-heart"></i></span>
+                <span className="time-notify">1h</span>
+                </div>
+              </li>
+              <li className="list-notify">
+                <img src="/images/resources/s1.png" alt = "logo" />
+                <div className="sgt-text">
+                <div className="detail-notify">
+                <h4>Jessica William</h4>
+                <p className="comment-notify"> đã thích bài viết của bạn</p>
+                </div>
+                <span><i className="fa fa-heart"></i></span>
+                <span className="time-notify">1h</span>
+                </div>
+              </li>
+              <li className="list-notify">
+                <img src="/images/resources/s1.png" alt = "logo" />
+                <div className="sgt-text">
+                <div className="detail-notify">
+                <h4>Jessica William</h4>
+                <p className="comment-notify"> đã thích bài viết của bạn</p>
+                </div>
+                <span><i className="fa fa-heart"></i></span>
+                <span className="time-notify">1h</span>
+                </div>
+              </li>
+              </a>
+            </ul>
+
+          </div>
+        </div>
+        </div>
+
       </React.Fragment>
     )
   }
