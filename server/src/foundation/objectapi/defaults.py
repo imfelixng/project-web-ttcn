d482c8b3bd1b7f5c24ac7f74cc1d8d00b6761292
Nguyen Phu <nvphu1306@gmail.com>
2018-12-17 18:39:34 +0700

    chore: update mqtt config

--- server/src/foundation/objectapi/defaults.py
+++ server/src/foundation/objectapi/defaults.py
@@ -29,3 +29,11 @@ X_DOMAINS = ['http://localhost:5000',  # The domain where Swagger UI is running
 X_HEADERS = ['Content-Type', 'If-Match']  # Needed for the "Try it out" buttons
 
 AUTH_FIELD = "auth"
+
+
+MQTT_BROKER_URL = 'localhost'  # use the free broker from HIVEMQ
+MQTT_BROKER_PORT = 1883  # default port for non-tls connection
+MQTT_USERNAME = ''  # set the username here if you need authentication for the broker
+MQTT_PASSWORD = ''  # set the password here if the broker demands authentication
+MQTT_KEEPALIVE = 5  # set the time interval for sending a ping to the broker to 5 seconds
+MQTT_TLS_ENABLED = False  # set TLS to disabled for testing purposes
